a991ddc49d402d59e80ab00ff1560acd62b1131a
Le-1v <timaleo03@gmail.com>
2024-08-08 16:01:05 +0300

    Add MAC-changer in Linux

---
+++ Linux/linuxMacChanger.py
@@ -0,0 +1,10 @@
+import subprocess
+
+def linuxMacChanger():
+    print('In Linux')
+    interface = input('Interface: ')
+    new_mac = input('New MAC-address: ')
+    subprocess.call(f"ifconfig {interface} down", shell=True)
+    subprocess.call(f"ifconfig {interface} hw ether {new_mac}", shell=True)
+    subprocess.call(f"ifconfig {interface} up", shell=True)
+    return 0
